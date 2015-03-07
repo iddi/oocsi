6f96dcdb37e0e242ce17671ab2dcfd65ec928a4a
matsfunk <mathias.funk@gmail.com>
2015-03-07 16:25:25 +0100

    OOCSI server version 1.0 - added OSC support and bundled library - added license file

--- server/src/nl/tue/id/oocsi/server/OOCSIServer.java
+++ server/src/nl/tue/id/oocsi/server/OOCSIServer.java
@@ -22,7 +22,7 @@ import com.google.gson.Gson;
 public class OOCSIServer extends Server {
 
 	// constants
-	public static final String VERSION = "0.9";
+	public static final String VERSION = "1.0";
 
 	// defaults for different services
 	public static int port = 4444;
