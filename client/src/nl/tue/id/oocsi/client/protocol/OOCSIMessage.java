a53e7b1bd32025b899b61a8d9dfc17a31121d4c4
matsfunk <mathias.funk@gmail.com>
2015-11-02 09:42:44 +0800

    extended the OOCSIMessage data method with more data types

--- client/src/nl/tue/id/oocsi/client/protocol/OOCSIMessage.java
+++ client/src/nl/tue/id/oocsi/client/protocol/OOCSIMessage.java
@@ -64,6 +64,36 @@ public class OOCSIMessage extends OOCSIEvent {
 	 * @param value
 	 * @return
 	 */
+	public OOCSIMessage data(String key, float value) {
+
+		// store data
+		this.data.put(key, value);
+
+		return this;
+	}
+
+	/**
+	 * store data in message
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public OOCSIMessage data(String key, double value) {
+
+		// store data
+		this.data.put(key, value);
+
+		return this;
+	}
+
+	/**
+	 * store data in message
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
 	public OOCSIMessage data(String key, long value) {
 
 		// store data
