9c0034cd39ba9cfd9c32a3dd9bca81583c71f205
matsfunk <mathias.funk@gmail.com>
2017-08-09 20:26:20 +0200

    client version 1.3

--- client/src/nl/tue/id/oocsi/client/OOCSIClient.java
+++ client/src/nl/tue/id/oocsi/client/OOCSIClient.java
@@ -16,7 +16,7 @@ import nl.tue.id.oocsi.client.socket.SocketClient;
  */
 public class OOCSIClient {
 
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.3";
 
 	private Map<String, Handler> channels = new HashMap<String, Handler>();
 	private Map<String, Responder> services = new HashMap<String, Responder>();
@@ -28,7 +28,6 @@ public class OOCSIClient {
 	/**
 	 * create OOCSI client with a RANDOM name as the system-wide handle
 	 * 
-	 * @param name
 	 */
 	public OOCSIClient() {
 		this(null);
