878bcdf7d5d90e9e0349ce6c29ef687f97bc8377
matsfunk <mathias.funk@gmail.com>
2017-11-28 12:20:58 +0100

    added load test for big messages

--- client/test/ClientLoadTest.java
+++ client/test/ClientLoadTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import nl.tue.id.oocsi.client.OOCSIClient;
 import nl.tue.id.oocsi.client.protocol.DataHandler;
+import nl.tue.id.oocsi.client.protocol.OOCSIMessage;
 
 import org.junit.Test;
 
@@ -63,6 +64,45 @@ public class ClientLoadTest {
 
 		assertEquals(1000, list.size());
 		assertEquals(list.get(1), "hello1");
+
+		o1.disconnect();
+		o2.disconnect();
+	}
+
+	@Test
+	public void testSendingBigMessages() throws InterruptedException {
+		final List<Object> list = new ArrayList<Object>();
+
+		OOCSIClient o1 = new OOCSIClient("test_client_big_message_1");
+		o1.connect("localhost", 4444);
+		assertTrue(o1.isConnected());
+		o1.subscribe(new DataHandler() {
+			public void receive(String sender, Map<String, Object> data, long timestamp) {
+				list.add((String) data.get("data"));
+			}
+		});
+
+		OOCSIClient o2 = new OOCSIClient("test_client_big_message_2");
+		o2.connect("localhost", 4444);
+		assertTrue(o2.isConnected());
+		o2.subscribe(new DataHandler() {
+			public void receive(String sender, Map<String, Object> data, long timestamp) {
+				list.add(data.get("load"));
+			}
+		});
+
+		int[][] largePayload = new int[10000][10];
+		for (int i = 0; i < 10000; i++) {
+			largePayload[i] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+		}
+		new OOCSIMessage(o1, "test_client_big_message_2").data("load", largePayload).send();
+
+		Thread.yield();
+		Thread.sleep(1000);
+
+		assertEquals(1, list.size());
+		assertTrue(((int[][]) list.get(0)).length == 10000);
+		assertEquals(3, ((int[][]) list.get(0))[0][2]);
 
 		o1.disconnect();
 		o2.disconnect();
