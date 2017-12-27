9fd7f69ec190b95d248fe6c69948cd32a4996a28
matsfunk <mathias.funk@gmail.com>
2017-12-27 11:12:35 +0100

    server version 1.8

--- server/src/nl/tue/id/oocsi/server/OOCSIServer.java
+++ server/src/nl/tue/id/oocsi/server/OOCSIServer.java
@@ -24,7 +24,7 @@ import nl.tue.id.oocsi.server.services.SocketService;
 public class OOCSIServer extends Server {
 
 	// constants
-	public static final String VERSION = "1.7";
+	public static final String VERSION = "1.8";
 
 	// defaults for different services
 	public static int port = 4444;
