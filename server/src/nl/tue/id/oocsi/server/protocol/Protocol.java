4bf596b73c9cbfa836cd5467369f68e833471500
matsfunk <mathias.funk@gmail.com>
2018-11-24 12:51:38 +0100

    - improved stability of message parsing

--- server/src/nl/tue/id/oocsi/server/protocol/Protocol.java
+++ server/src/nl/tue/id/oocsi/server/protocol/Protocol.java
@@ -93,77 +93,8 @@ public class Protocol {
 				String message = tokens[2];
 
 				Channel c = server.getChannel(recipient);
-				if (c != null) {
-					Map<String, Object> map = new HashMap<String, Object>();
-
-					// try to parse input as JSON
-					try {
-						Gson serializer = new Gson();
-						JsonElement je = new JsonParser().parse(message);
-						if (je.isJsonObject()) {
-							JsonObject jo = je.getAsJsonObject();
-							for (Map.Entry<String, JsonElement> element : jo.entrySet()) {
-								// translate primitives from json to java
-								JsonElement value = element.getValue();
-								// element is a single primitive
-								if (value.isJsonPrimitive()) {
-									map.put(element.getKey(), value);
-								}
-								// element is an array
-								else if (value.isJsonArray()) {
-									JsonArray asJsonArray = value.getAsJsonArray();
-
-									// array is non-empty and contains primitives
-									if (asJsonArray != null && asJsonArray.size() > 0
-											&& asJsonArray.get(0).isJsonPrimitive()) {
-
-										// booleans
-										if (asJsonArray.get(0).getAsJsonPrimitive().isBoolean()) {
-											boolean[] array = new boolean[asJsonArray.size()];
-											for (int i = 0; i < array.length; i++) {
-												JsonElement jsonElement = asJsonArray.get(i);
-												if (jsonElement.isJsonPrimitive()
-														&& jsonElement.getAsJsonPrimitive().isBoolean()) {
-													array[i] = jsonElement.getAsBoolean();
-												}
-											}
-											map.put(element.getKey(), array);
-										}
-										// numbers
-										else if (asJsonArray.get(0).getAsJsonPrimitive().isNumber()) {
-											float[] array = new float[asJsonArray.size()];
-											for (int i = 0; i < array.length; i++) {
-												JsonElement jsonElement = asJsonArray.get(i);
-												if (jsonElement.isJsonPrimitive()
-														&& jsonElement.getAsJsonPrimitive().isNumber()) {
-													array[i] = jsonElement.getAsFloat();
-												}
-											}
-											map.put(element.getKey(), array);
-										}
-										// string
-										else if (asJsonArray.get(0).getAsJsonPrimitive().isString()) {
-											String[] array = new String[asJsonArray.size()];
-											for (int i = 0; i < array.length; i++) {
-												JsonElement jsonElement = asJsonArray.get(i);
-												if (jsonElement.isJsonPrimitive()
-														&& jsonElement.getAsJsonPrimitive().isString()) {
-													array[i] = jsonElement.getAsString();
-												}
-											}
-											map.put(element.getKey(), array);
-										}
-									} else if (!value.isJsonNull()) {
-										map.put(element.getKey(), serializer.toJson(value));
-									}
-								}
-							}
-						}
-					} catch (JsonSyntaxException jse) {
-						// in case of problems, add the full data as a raw string
-						map.put("data", message);
-					}
-
+				if (c != null && c.accept(recipient)) {
+					Map<String, Object> map = parseJSONMessage(message);
 					c.send(new Message(sender.getName(), recipient, new Date(), map));
 				}
 			}
@@ -175,32 +106,120 @@ public class Protocol {
 				String recipient = tokens[1];
 				String data = tokens[2];
 
-				try {
-					// serialized object parsing
-					ByteArrayInputStream bais = new ByteArrayInputStream(Base64Coder.decode(data));
-					ObjectInputStream ois = new ObjectInputStream(bais);
-					Object outputObject = ois.readObject();
+				Channel c = server.getChannel(recipient);
+				if (c != null && c.accept(recipient)) {
+					try {
+						Object outputObject = parseJavaMessage(data);
 
-					Channel c = server.getChannel(recipient);
-					if (c != null && c.accept(recipient)) {
 						@SuppressWarnings("unchecked")
 						Map<String, Object> map = (Map<String, Object>) outputObject;
 						c.send(new Message(sender.getName(), recipient, new Date(), map));
+					} catch (IOException e) {
+						OOCSIServer.log("[MsgParser] I/O problem: " + e.getMessage());
+					} catch (IllegalArgumentException e) {
+						OOCSIServer.log("[MsgParser] Base64 encoder problem: " + e.getMessage());
+					} catch (ClassNotFoundException e) {
+						OOCSIServer.log("[MsgParser] Unknown class: " + e.getMessage());
+					} catch (Exception e) {
+						// just in case
+						OOCSIServer.log("[MsgParser] Unknown problem: " + e.getMessage());
 					}
-				} catch (IOException e) {
-					OOCSIServer.log("[MsgParser] I/O problem: " + e.getMessage());
-				} catch (IllegalArgumentException e) {
-					OOCSIServer.log("[MsgParser] Base64 encoder problem: " + e.getMessage());
-				} catch (ClassNotFoundException e) {
-					OOCSIServer.log("[MsgParser] Unknown class: " + e.getMessage());
-				} catch (Exception e) {
-					// just in case
-					OOCSIServer.log("[MsgParser] Unknown problem: " + e.getMessage());
 				}
 			}
 		}
 
 		// default response
 		return "";
+	}
+
+	/**
+	 * parse a JSON message
+	 * 
+	 * @param message
+	 * @return
+	 */
+	public static Map<String, Object> parseJSONMessage(String message) {
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		// try to parse input as JSON
+		try {
+			Gson serializer = new Gson();
+			JsonElement je = new JsonParser().parse(message);
+			if (je.isJsonObject()) {
+				JsonObject jo = je.getAsJsonObject();
+				for (Map.Entry<String, JsonElement> element : jo.entrySet()) {
+					// translate primitives from json to java
+					JsonElement value = element.getValue();
+					// element is a single primitive
+					if (value.isJsonPrimitive()) {
+						map.put(element.getKey(), value);
+					}
+					// element is an array
+					else if (value.isJsonArray()) {
+						JsonArray asJsonArray = value.getAsJsonArray();
+
+						// array is non-empty and contains primitives
+						if (asJsonArray != null && asJsonArray.size() > 0 && asJsonArray.get(0).isJsonPrimitive()) {
+
+							// booleans
+							if (asJsonArray.get(0).getAsJsonPrimitive().isBoolean()) {
+								boolean[] array = new boolean[asJsonArray.size()];
+								for (int i = 0; i < array.length; i++) {
+									JsonElement jsonElement = asJsonArray.get(i);
+									if (jsonElement.isJsonPrimitive() && jsonElement.getAsJsonPrimitive().isBoolean()) {
+										array[i] = jsonElement.getAsBoolean();
+									}
+								}
+								map.put(element.getKey(), array);
+							}
+							// numbers
+							else if (asJsonArray.get(0).getAsJsonPrimitive().isNumber()) {
+								float[] array = new float[asJsonArray.size()];
+								for (int i = 0; i < array.length; i++) {
+									JsonElement jsonElement = asJsonArray.get(i);
+									if (jsonElement.isJsonPrimitive() && jsonElement.getAsJsonPrimitive().isNumber()) {
+										array[i] = jsonElement.getAsFloat();
+									}
+								}
+								map.put(element.getKey(), array);
+							}
+							// string
+							else if (asJsonArray.get(0).getAsJsonPrimitive().isString()) {
+								String[] array = new String[asJsonArray.size()];
+								for (int i = 0; i < array.length; i++) {
+									JsonElement jsonElement = asJsonArray.get(i);
+									if (jsonElement.isJsonPrimitive() && jsonElement.getAsJsonPrimitive().isString()) {
+										array[i] = jsonElement.getAsString();
+									}
+								}
+								map.put(element.getKey(), array);
+							}
+						} else if (!value.isJsonNull()) {
+							map.put(element.getKey(), serializer.toJson(value));
+						}
+					}
+				}
+			}
+		} catch (JsonSyntaxException jse) {
+			// in case of problems, add the full data as a raw string
+			map.put("data", message);
+		}
+		return map;
+	}
+
+	/**
+	 * parse a Java message
+	 * 
+	 * @param data
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public static Object parseJavaMessage(String data) throws IOException, ClassNotFoundException {
+		// serialized object parsing
+		ByteArrayInputStream bais = new ByteArrayInputStream(Base64Coder.decode(data));
+		ObjectInputStream ois = new ObjectInputStream(bais);
+		Object outputObject = ois.readObject();
+		return outputObject;
 	}
 }
