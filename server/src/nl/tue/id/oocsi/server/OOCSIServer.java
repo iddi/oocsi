e32cf6947751d0b1e888446d807fd27f2e4fa38c
matsfunk <mathias.funk@gmail.com>
2020-03-26 20:09:06 +0100

    v1.17  - fixed jar server bundling for Java 11 & 13

--- server/src/nl/tue/id/oocsi/server/OOCSIServer.java
+++ server/src/nl/tue/id/oocsi/server/OOCSIServer.java
@@ -26,7 +26,7 @@ import nl.tue.id.oocsi.server.services.SocketService;
 public class OOCSIServer extends Server {
 
 	// constants
-	public static final String VERSION = "1.16";
+	public static final String VERSION = "1.17";
 
 	// defaults for different services
 	private static int maxClients = 100;
