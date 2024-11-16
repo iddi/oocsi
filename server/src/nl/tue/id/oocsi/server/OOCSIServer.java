50062565eff895ccbad4a2b39ca59c711707cce3
matsfunk <mathias.funk@gmail.com>
2024-11-16 12:32:05 +0100

    v1.3.2

--- server/src/nl/tue/id/oocsi/server/OOCSIServer.java
+++ server/src/nl/tue/id/oocsi/server/OOCSIServer.java
@@ -29,7 +29,7 @@ import nl.tue.id.oocsi.server.services.PresenceTracker;
 public class OOCSIServer extends Server {
 
 	// constants
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2";
 
 	// defaults for different services
 	private int maxClients = 100;
